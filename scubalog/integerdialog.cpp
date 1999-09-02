e5e10bc51f1430901e20d934e00ddd1e74d0a395
André Hübert Johansen <andrejoh@gmail.com>
1999-09-02 19:21:16 +0000

    Dialog used to input an integer (within an optional range).

---
+++ scubalog/integerdialog.cpp
@@ -0,0 +1,209 @@
+//*****************************************************************************
+/*!
+  \file ScubaLog/widgets/integerdialog.cpp
+  \brief This file contains the implementation of IntegerDialog.
+
+  $Id$
+
+  \par Copyright:
+  André Johansen.
+*/
+//*****************************************************************************
+
+#include <assert.h>
+#include <qdialog.h>
+#include <qlabel.h>
+#include <qpushbutton.h>
+#include <qlayout.h>
+#include "kintegeredit.h"
+#include "integerdialog.h"
+#include "integerdialog.moc"
+
+
+//*****************************************************************************
+/*!
+  Create the dialog object with \a pcParent as parent widget and
+  \a pzName as widget name. If \a isModal is `true', the dialog
+  will be modal. The flags \a nFlags can be used to customize the dialog.
+
+  The dialog will by default accept all possible integers. Use setMinValue()
+  and setMaxValue() to restrict the range accepted.
+
+  To get a label above the integer editor, use setText().
+
+  \author André Johansen.
+*/
+//*****************************************************************************
+
+IntegerDialog::IntegerDialog(QWidget* pcParent, const char* pzName,
+                             bool isModal, WFlags nFlags)
+  : QDialog(pcParent, pzName, isModal, nFlags),
+    m_pcText(0),
+    m_pcEditor(0),
+    m_pcOk(0),
+    m_pcCancel(0)
+{
+  m_pcText = new QLabel(this, "text");
+  m_pcText->setAlignment(AlignHCenter|AlignVCenter);
+  m_pcText->setAutoResize(true);
+
+  m_pcEditor = new KIntegerEdit(this, "integerEditor");
+  m_pcEditor->setFocus();
+  m_pcEditor->setMinimumSize(m_pcEditor->sizeHint());
+
+  m_pcOk = new QPushButton(this, "okButton");
+  m_pcOk->setText("&Ok");
+  m_pcOk->setDefault(true);
+  connect(m_pcOk, SIGNAL(clicked()), SLOT(accept()));
+
+  m_pcCancel = new QPushButton(this, "cancelButton");
+  m_pcCancel->setText("&Cancel");
+  connect(m_pcCancel, SIGNAL(clicked()), SLOT(reject()));
+
+  QSize cButtonSize(m_pcCancel->sizeHint());
+  if ( m_pcOk->sizeHint().width() > cButtonSize.width() )
+    cButtonSize.setWidth(m_pcOk->sizeHint().width());
+  m_pcOk->setMinimumSize(cButtonSize);
+  m_pcCancel->setMinimumSize(cButtonSize);
+
+  QGridLayout* pcLayout = new QGridLayout(this, 3, 3);
+  pcLayout->setColStretch(1, 1);
+  pcLayout->setRowStretch(0, 1);
+  pcLayout->addMultiCellWidget(m_pcText,   0, 0, 0, 2);
+  pcLayout->addMultiCellWidget(m_pcEditor, 1, 1, 0, 2);
+  pcLayout->addWidget(m_pcOk,     2, 0);
+  pcLayout->addWidget(m_pcCancel, 2, 2);
+  pcLayout->activate();
+
+  updateGeometry();
+}
+
+
+//*****************************************************************************
+/*!
+  Destroy the dialog.
+
+  \author André Johansen.
+*/
+//*****************************************************************************
+
+IntegerDialog::~IntegerDialog()
+{
+  m_pcText   = 0;
+  m_pcEditor = 0;
+  m_pcOk     = 0;
+  m_pcCancel = 0;
+}
+
+
+//*****************************************************************************
+/*!
+  Set the value to be shown to \a nValue.
+
+  \sa KIntegerEdit::setValue().
+
+  \author André Johansen.
+*/
+//*****************************************************************************
+
+void
+IntegerDialog::setValue(int nValue)
+{
+  m_pcEditor->setValue(nValue);
+}
+
+
+//*****************************************************************************
+/*!
+  Set the minimum acceptable value to \a nMinValue.
+
+  \sa KIntegerEdit::setMinValue().
+
+  \author André Johansen.
+*/
+//*****************************************************************************
+
+void
+IntegerDialog::setMinValue(int nMinValue)
+{
+  m_pcEditor->setMinValue(nMinValue);
+}
+
+
+//*****************************************************************************
+/*!
+  Set the maximum acceptable value to \a nMaxValue.
+
+  \sa KIntegerEdit::setMaxValue().
+
+  \author André Johansen.
+*/
+//*****************************************************************************
+
+void
+IntegerDialog::setMaxValue(int nMaxValue)
+{
+  m_pcEditor->setMaxValue(nMaxValue);
+}
+
+
+//*****************************************************************************
+/*!
+  Get the current value from the integer editor.
+
+  \author André Johansen.
+*/
+//*****************************************************************************
+
+int
+IntegerDialog::getValue() const
+{
+  return m_pcEditor->getValue();
+}
+
+
+//*****************************************************************************
+/*!
+  Set the text to be shown above the editor to \a cText.
+
+  \author André Johansen.
+*/
+//*****************************************************************************
+
+void
+IntegerDialog::setText(const QString& cText)
+{
+  m_pcText->setText(cText);
+  m_pcText->setMinimumSize(m_pcText->sizeHint());
+  updateGeometry();
+}
+
+
+//*****************************************************************************
+/*!
+  Update the geometry.
+
+  \author André Johansen.
+*/
+//*****************************************************************************
+
+void
+IntegerDialog::updateGeometry()
+{
+  int nMinWidth  = QMAX(m_pcEditor->minimumSize().width(),
+                        m_pcOk->minimumSize().width() +
+                        m_pcCancel->minimumSize().width());
+  int nMinHeight = (m_pcText->minimumSize().width() +
+                    m_pcEditor->minimumSize().height() +
+                    m_pcOk->minimumSize().height());
+  setMinimumSize(nMinWidth, nMinHeight);
+  resize(nMinWidth, nMinHeight);
+}
+
+
+// Local Variables:
+// mode: c++
+// tab-width: 8
+// c-basic-offset: 2
+// indent-tabs-mode: nil
+// End:
---
+++ scubalog/integerdialog.h
@@ -0,0 +1,83 @@
+//*****************************************************************************
+/*!
+  \file ScubaLog/widgets/integerdialog.h
+  \brief This file contains the definition of the IntegerDialog class.
+
+  $Id$
+
+  \par Copyright:
+  André Johansen.
+*/
+//*****************************************************************************
+
+#ifndef INTEGERDIALOG_H
+#define INTEGERDIALOG_H
+
+#include <qdialog.h>
+
+
+class QString;
+class QLabel;
+class QPushButton;
+class KIntegerEdit;
+
+
+//*****************************************************************************
+/*!
+  \class IntegerDialog
+  \brief The IntegerDialog class is used to prompt the user for an integer.
+
+  This dialog is used to ask a user about an integer, which can be forced
+  to stay within a user specified range by calling setMinValue() and
+  setMaxValue().
+
+  The default integer to be shown can be set with setValue().
+
+  A text to be shown above the editor can be set with setLabel().
+
+  The function getValue() can be used at any time to query the currently
+  entered integer.
+
+  \sa KIntegerEdit.
+
+  \author André Johansen.
+*/
+//*****************************************************************************
+
+class IntegerDialog : public QDialog {
+  Q_OBJECT
+public:
+  IntegerDialog(QWidget* pcParent = 0, const char* pzName = 0,
+                bool isModal = false, WFlags nFlags = 0);
+  virtual ~IntegerDialog();
+
+  void setValue(int nValue);
+  void setMinValue(int nMinValue);
+  void setMaxValue(int nMaxValue);
+
+  void setText(const QString& cText);
+
+  int getValue() const;
+
+protected:
+  void updateGeometry();
+
+  //! The label.
+  QLabel*       m_pcText;
+  //! The integer editor line.
+  KIntegerEdit* m_pcEditor;
+  //! The ok button.
+  QPushButton*  m_pcOk;
+  //! The cancel button.
+  QPushButton*  m_pcCancel;
+};
+
+
+#endif // INTEGERDIALOG_H
+
+// Local Variables:
+// mode: c++
+// tab-width: 8
+// c-basic-offset: 2
+// indent-tabs-mode: nil
+// End:
